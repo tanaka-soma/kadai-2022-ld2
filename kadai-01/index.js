6efb515fc96de1ada518ac3321b4e6d7835d1ed4
tanaka-soma <tanaka.bbx@gmail.com>
2022-10-19 05:02:12 +0000

    https://github.com/tanaka-soma

--- kadai-01/index.js
+++
@@ -1,3 +0,0 @@
-console.log("これは人文学のための情報処理Ⅱのやつですね")
-console.log("aaaaa")
-    
---
+++ kadai-02/index.js
@@ -0,0 +1,12 @@
+const firstname ='souma'
+const familyname='tanaka'
+console.log(firstname+familyname
+)
+const num=1+2+3+4+5+6+7+8+9+10
+console.log(num)
+const dog=true
+const cat=false
+console.log("犬または猫を飼ってる",dog || cat)
+console.log("犬と猫の両方を狩っている",dog && cat)
+console.log("犬も猫も飼ってない", !dog && !cat)
+console.log("犬も猫も飼っていない",!(dog||cat))
