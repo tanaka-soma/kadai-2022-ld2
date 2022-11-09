f2e332549147b3c97c51222855eb821f5de53722
tanaka-soma <tanaka.bbx@gmail.com>
2022-11-09 05:31:13 +0000

    課題④

--- kadai-04 /index.js
+++
@@ -1,5 +0,0 @@
-
-const hello=(name)=>{
-    console.log("こんにちわ"+name)
-}
-hello()
---
+++ kadai-04 /index2.js
@@ -0,0 +1,5 @@
+setTimeout(()=>{ 
+    console.log('Hello!')
+    setTimeout(()=>{console.log('Bye!')}, 2000)
+}
+,3000)
